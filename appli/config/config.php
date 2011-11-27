a217be26e4df182b06228f35406445ab132304f7
SirLouen <sir.louen@gmail.com>
2011-11-27 13:31:25 +0100

    pruebas

--- appli/config/config.php
+++ appli/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://ibericarcadiz.com/automotis/v2/';
+$config['base_url']	= 'http://localhost/automotis2/';
+//$config['base_url']	= 'http://ibericarcadiz.com/automotis/v2/';
 
 /*
 |--------------------------------------------------------------------------
---
+++ appli/controllers/blog.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Created on 27/11/2011
+ *
+ * Automotis v2 DMS  Copyright (C) <2011>  mcamargo
+ * This program comes with ABSOLUTELY NO WARRANTY; 
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions; Read README file for more information
+
+ */
+ 
+ class Blog extends CI_Controller
+ {
+ 	public function index()
+ 	{
+ 		echo "Hola Mundo!";
+ 	}
+ 	
+ 	public function comentarios()
+ 	{
+ 		echo "Mire esto!";
+ 	}
+ }
+ 
+?>
